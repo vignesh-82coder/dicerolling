6a3e7699b135911b0b9fc499a11e5b405c1b0bf0
vignesh-82coder <61943147+vignesh-82coder@users.noreply.github.com>
2020-08-10 12:22:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var randomnumber1=Math.floor(Math.random()*6)+1;
+var randomDiceImage="dice" + randomnumber1 + ".png";
+var randomImageSource ="images/" + randomDiceImage;
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+var randomnumber2=Math.floor(Math.random()*6)+1;
+var randomImageSource2 ="images/dice" + randomnumber2 +".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
